8cbe102ac4aec506557b5dfd1ceba75d90d37eba
xiaobing.zhang <xiaobing.zhang@intel.com>
2019-05-13 09:46:20 +0800

    Add mkldnn mul

--- aten/src/ATen/native/mkldnn/BinaryOps.cpp
+++ aten/src/ATen/native/mkldnn/BinaryOps.cpp
@@ -1,6 +1,7 @@
 #include <ATen/ATen.h>
 #include <ATen/Config.h>
 #include <ATen/NativeFunctions.h>
+#include <ATen/Parallel.h>
 
 #if !AT_MKLDNN_ENABLED()
 
@@ -21,6 +22,18 @@ Tensor mkldnn_add(const Tensor& self, const Tensor& other, Scalar alpha) {
 
 Tensor& mkldnn_add_(Tensor& self, const Tensor& other, Scalar alpha) {
   AT_ERROR("mkldnn_add_: ATen not compiled with MKLDNN support");
+}
+
+Tensor& mkldnn_mul_out(Tensor& result, const Tensor& self, const Tensor& other) {
+  AT_ERROR("mkldnn_mul_out: ATen not compiled with MKLDNN support");
+}
+
+Tensor mkldnn_mul(const Tensor& self, const Tensor& other) {
+  AT_ERROR("mkldnn_mul: ATen not compiled with MKLDNN support");
+}
+
+Tensor& mkldnn_mul_(Tensor& self, const Tensor& other) {
+   AT_ERROR("mkldnn_mul_: ATen not compiled with MKLDNN support");
 }
 
 } // namespace native
@@ -63,6 +76,40 @@ Tensor& mkldnn_add_(Tensor& self, const Tensor& other, Scalar alpha) {
   return native::mkldnn_add_out(self, self, other, alpha);
 }
 
+Tensor& mkldnn_mul_out(Tensor& result, const Tensor& self, const Tensor& other) {
+  auto n = self.numel();
+
+  ideep::tensor& z = itensor_from_mkldnn(result);
+  ideep::tensor& x = itensor_from_mkldnn(self);
+
+  auto* z_ = static_cast<float *>(z.get_data_handle());
+  auto* x_ = static_cast<float *>(x.get_data_handle());
+
+  float* y_;
+  bool is_scalar = true;
+  if (other.ndimension() != 0) { 
+    is_scalar = false;
+    ideep::tensor& y = itensor_from_mkldnn(other);
+    y_ = static_cast<float *>(y.get_data_handle());
+  }
+
+  parallel_for(0, n, 1, [=](int64_t begin, int64_t end){
+    for (int64_t index = begin; index < end; index++) {
+      z_[index] = x_[index] * (is_scalar ?  other.item().to<float>(): y_[index]);
+    }
+  });
+
+  return result;
+}
+
+Tensor mkldnn_mul(const Tensor& self, const Tensor& other) {
+  Tensor result = new_with_sizes_mkldnn(self.sizes(), self.options());
+  return native::mkldnn_mul_out(result, self, other);
+}
+
+Tensor& mkldnn_mul_(Tensor& self, const Tensor& other) {
+  return native::mkldnn_mul_out(self, self, other);
+}
 
 } // namespace native
 } // namespace at
